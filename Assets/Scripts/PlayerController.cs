b71e97cf102eec70ff24067ec030582ee5c6ef4b
WootenWarrior <152801888+WootenWarrior@users.noreply.github.com>
2024-02-07 17:24:31 +0000

    More map editing

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public List<GameObject>[] territories;
+    public GameObject PlayerObject;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
