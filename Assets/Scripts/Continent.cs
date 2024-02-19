4432ee90f2770998a1efcdd0783f488e55068f8b
WootenWarrior <152801888+WootenWarrior@users.noreply.github.com>
2024-02-19 18:02:41 +0000

    More game manager updates
    Working on instantiation of territories

--- Assets/Scripts/Continent.cs
+++ Assets/Scripts/Continent.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Continent : Territory
 {
     public List<Territory> territories;   //List of all the territory objects in the continent (found in the hierarchy)
-    private void Start() {
+    private void Awake() {
         foreach (Transform child in transform)
         {
-            Territory territory = child.GetComponent<Territory>();
+            Territory territory = child.AddComponent<Territory>();
             territories.Add(territory);
         }
     }
