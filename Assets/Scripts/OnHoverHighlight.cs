bbf3fca5461fff9cb88bd4d8e8f1e523a29ed21d
WootenWarrior <152801888+WootenWarrior@users.noreply.github.com>
2024-02-25 16:17:32 +0000

    re-doing the sprites for the territories

--- Assets/Scripts/OnHoverHighlight.cs
+++ Assets/Scripts/OnHoverHighlight.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PolygonCollider2D))]
 public class OnHoverHighlight : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
