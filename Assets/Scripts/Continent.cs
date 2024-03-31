373efca26a6c5a30efcf31dfbf548a6307f08962
WootenWarrior <152801888+WootenWarrior@users.noreply.github.com>
2024-03-31 17:54:00 +0100

    Minor bug fix

--- Assets/Scripts/Continent.cs
+++ Assets/Scripts/Continent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Continent : Territory
+public class Continent : MonoBehaviour
 {
     public List<Territory> territories;   //List of all the territory objects in the continent (found in the hierarchy)
 }
