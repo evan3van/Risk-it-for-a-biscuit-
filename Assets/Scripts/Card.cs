a371253a3c5470dfc2675e15891587050ef61e33
evan3van <eb690@sussex.ac.uk>
2024-04-23 16:50:59 +0100

    card menu

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Card : MonoBehaviour
 {
+
+    public void GoTocardMenu()
+    {
+        SceneManager.LoadScene("cardMenu");
+    }
+
+    public void GoTocanvasMenu()
+    {
+        SceneManager.LoadScene("canvas");
+    }
+
+
     public List <GameObject> cardList;
+
+    public List <GameObject> displayCards;
+
+    
 
 
 }
