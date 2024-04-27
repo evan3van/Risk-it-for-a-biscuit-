fdcf59d6e83dddb900e9ba60d7a08b5a10f98f5c
WootenWarrior <152801888+WootenWarrior@users.noreply.github.com>
2024-04-27 18:17:13 +0100

    Working on card selection / deselection

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,6 +11,7 @@ public class CardManager : MonoBehaviour
     
     public Turn turn;
     public List<Card> selectedCards = new();
+    public List<TextMeshProUGUI> selectedCardsText;
     private void Start() 
     {
         foreach (GameObject card in cardList)
@@ -39,4 +42,39 @@ public class CardManager : MonoBehaviour
     public List <GameObject> cardList;
 
     public List <GameObject> displayCards;
+
+    public void CardSelect(UnityEngine.UI.Image image)
+    {
+        Sprite imageSprite = image.sprite;
+        bool select = true;
+        foreach (GameObject card in cardList)
+        {
+            if(card.GetComponent<SpriteRenderer>().sprite == imageSprite && selectedCards.Count < 3)
+            {
+                foreach (Card selectedCard in selectedCards)
+                {
+                    if (selectedCard == card.GetComponent<Card>())
+                    {
+                        select = false;
+                    }
+                }
+                if (select)
+                {
+                    image.color = Color.red;
+                    selectedCards.Add(card.GetComponent<Card>());
+                }
+                else
+                {
+                    image.color = Color.white;
+                    selectedCards.Remove(card.GetComponent<Card>());
+                }
+            }
+            else if (selectedCards.Count >=3)
+            {
+                image.color = Color.red;
+                selectedCards.RemoveAt(2);
+                selectedCards.Add(card.GetComponent<Card>());
+            }
+        }
+    }
 }
